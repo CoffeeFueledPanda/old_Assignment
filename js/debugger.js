48bbf6d4ec2ac33ac3fbc86f8709b5b4214901ed
Coffee_Fueled_Panda <shieldforthesmirkingking@gmail.com>
2019-08-09 21:09:43 +0200

    First upload

---
+++ js/debugger.js
@@ -0,0 +1,11 @@
+// Good tool to find those pesky little creators of horizontal scrollbar hell.... =()
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
